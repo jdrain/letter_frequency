340fbb1e83ff8f8279956185ccace75ac070ae9e
jdrain <jdrain@email.sc.edu>
2017-04-12 11:38:15 -0400

    adding capabilities for a breadth first traversal

--- huffman_tree.py
+++ huffman_tree.py
@@ -4,6 +4,7 @@ class huffman_tree:
         self.root = root
 
     def DFS(self, root):
+        # bit of a misnomer right now. More so a depth first traversal
         ls = [root]
         if self.left != None:
             dfs(self.left)
@@ -17,6 +18,20 @@ class huffman_tree:
             dfs(current_node.left)
         if current_node.right != None:
             dfs(current_node.right)
+
+    def BFT(self):
+        next_round = self.root.get_children()
+        next_round_tmp = []
+        while next_round != []:
+            del next_round_tmp[:]
+            for n in next_round:
+                #keep traversing
+                for i in n.get_children():
+                    print("KEY: %s VAL: %d" % (i.data[0],i.data[1]))
+                    next_round_tmp.append(i)
+            del next_round[:]
+            for i in next_round_tmp:
+                next_round.append(i)
 
     def construct_from_list_of_tuples(self,tuple_ls):
         # the second tuple val should be the one with the weighting
@@ -60,13 +75,21 @@ class node:
     def __init__(self,tuple_data):
         self.data = tuple_data
         self.right = None
-        self.right = None
+        self.left = None
 
     def set_right(self,right):
         self.right = right
 
     def set_left(self,left):
         self.left = left
+
+    def get_children(self):
+        ls = []
+        if self.right != None:
+            ls.append(self.right)
+        if self.left != None:
+            ls.append(self.left)
+        return ls
 
     def has_children(self):
         if self.right == None and self.right == None:
