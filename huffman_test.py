60bf8a023ab9ccbe8add2320561d6b5b77a5a702
jdrain <jdrain@email.sc.edu>
2017-04-12 11:37:51 -0400

    adding testing for the traversal now

--- huffman_test.py
+++ huffman_test.py
@@ -2,7 +2,7 @@ from huffman_tree import node, huffman_tree
 
 #used to count the frequency of letters in a large text file
 
-count = 0.0
+count = 0
 letter_freq = {}
 with open("big.txt","r") as f:
     for line in f:
@@ -13,13 +13,13 @@ with open("big.txt","r") as f:
             else:
                 #initialize the key
                 letter_freq[key] = 1
-            count += 1.0
+            count += 1
     count_ls = [(key,letter_freq[key]) for key in letter_freq.keys()]
 
 #now sort the key - val pairs as a list of tuples
 count_ls.sort(key=lambda tup: tup[1], reverse=True)
 for i in count_ls:
-    print("KEY: %s, VAL: %f" % (i[0],i[1]))
+    print("KEY: %s, VAL: %d" % (i[0],i[1]))
 
 #number of characters
 print("char volume: %d" % len(count_ls))
@@ -27,3 +27,4 @@ print("char volume: %d" % len(count_ls))
 rt = node((None,None))
 t = huffman_tree(rt)
 t.construct_from_list_of_tuples(count_ls)
+t.BFT()
